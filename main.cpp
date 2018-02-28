e40d795de45aad425a4a8b1d112b61c1e3db9c27
tomes <32511604+Tomes0@users.noreply.github.com>
2018-02-28 12:08:34 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,8 +1,49 @@
 #include <iostream>
-
+#include <string>
 using namespace std;
 
-int main(){
-	cout<<"Good morning";
-	return 0;
+int defaultname(string name){
+    if(name.length())
+        return 1;
+        else return 0;
+}
+
+void namekiir(string name){
+    for(int i=0;i<39;i++)cout<<' ';
+    cout << "Good morning, " ;
+    if(defaultname(name))cout<<name;
+        else cout<<" my friend!";
+    cout<<endl;
+}
+
+void hisvet(string name){
+    for(int i=0;i<39;i++)cout<<' ';
+    cout<< "Happy eastern, ";
+    if(defaultname(name))cout<<name;
+        else cout<<" my friend!";
+    cout<<endl;
+}
+
+void keret(){
+    for(int i=0;i<80;i++)cout<<"*";
+}
+void csunyakeret(){
+    cout<<endl;
+    cout<<'*';
+    for(int i=0;i<78;i++)cout<<' ';
+    cout<<'*';
+}
+int main()
+{
+    string name;
+    getline(cin,name);
+    keret();
+    csunyakeret();
+    cout<<endl;
+    namekiir(name);
+    hisvet(name);
+    csunyakeret();
+    keret();
+
+    return 0;
 }
